07d1a8255da93b564215fbfd76fd0dd0d2564ef3
eb518 <99305740+eb518@users.noreply.github.com>
2022-02-15 20:19:57 -0600

    Add files via upload
    Pizza price formula

---
+++ Untitled.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+At Angelo’s Pizza Wheels, you have: 
+Three sauce options (red, white, and pesto)
+Three cheese options (mozzarella, feta, and ricotta)
+Eight toppings (olives, mushrooms, peppers, pepperoni, sausage, meatballs, arugula, and prosciutto)
+Two glazes (olive oil and balsamic)
+premium toppings (burrata, avocado, and BBQ chicken)
+
+
+# In[15]:
+
+
+sauce= "red"
+cheese="mozzarella"
+glaze="olive oil"
+toppings= 2
+#olives and mushrooms
+premium_toppings=1
+#BBQ chicken
+
+
+# In[16]:
+
+
+total=8+(toppings*.5)+(premium_toppings*2)
+print(total)
+
+
+# In[ ]:
+
+
+
+
