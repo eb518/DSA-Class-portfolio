92df0783f5c7a62a6f8c2c04c5086f8647acd9e8
eb518 <99305740+eb518@users.noreply.github.com>
2022-02-15 19:03:59 -0600

    Add files via upload

---
+++ coffee prices.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num_latte=10
+num_mocha=7
+num_coffee=12
+
+
+# In[2]:
+
+
+price_latte=4
+price_mocha=4.25
+price_coffee=2
+
+
+# In[3]:
+
+
+total_latte=num_latte*price_latte
+print(total_latte)
+
+
+# In[4]:
+
+
+total_mocha=num_mocha*price_mocha
+print(total_mocha)
+
+
+# In[5]:
+
+
+total_coffee=num_coffee*price_coffee
+print(total_coffee)
+
+
+# In[6]:
+
+
+grand_total=total_latte+total_mocha+total_coffee
+print(grand_total)
+
+
+# In[ ]:
+
+
+
+
